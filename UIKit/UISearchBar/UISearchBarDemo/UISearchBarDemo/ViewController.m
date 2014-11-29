c7aaf00a4e55d189c2eea6d777f5a7d0a6b1d3e5
mytcer <woaibou@163.com>
2014-11-29 14:49:47 +0800

    增加UISearchBar

--- UIKit/UISearchBar/UISearchBarDemo/UISearchBarDemo/ViewController.m
+++ UIKit/UISearchBar/UISearchBarDemo/UISearchBarDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property UILabel *label;
 @end
 
 @implementation ViewController
@@ -17,23 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CGSize viewSize = self.view.bounds.size;
+    // 实例并初始化
     UISearchBar *searchBar = [[UISearchBar alloc] init];
-    searchBar.frame = CGRectMake((viewSize.width - 200) / 2, 50, 200, 50);
+    // 设置位置及尺寸
+    searchBar.frame = CGRectMake((self.view.bounds.size.width - 300) / 2, 50, 300, 50);
     // 占位符
     searchBar.placeholder = @"Search";
-    // 默认值
-    searchBar.text = @"haha";
-    // 设置背景色
-    searchBar.barTintColor = [UIColor clearColor];
-    // 该属性用于指定搜索条是否有背景，UISearchBarStyleMinimal表示没有背景，且搜索区是半透明的
+    // 指定搜索条的外观，UISearchBarStyleMinimal表示没有背景，且搜索区是半透明的
     searchBar.searchBarStyle = UISearchBarStyleMinimal;
+    // 设置委托对象
+    searchBar.delegate = self;
+    
+    // 文本
+    self.label = [[UILabel alloc] init];
+    self.label.frame = CGRectMake(0, 0, 200, 50);
+    self.label.center = self.view.center;
+    self.label.text = @"这里会显示搜索框中输入的文本";
+    self.label.textAlignment = NSTextAlignmentCenter;
+    
     [self.view addSubview:searchBar];
+    [self.view addSubview:self.label];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    self.label.text = [NSString stringWithFormat:@"你搜索是：%@", searchText];
 }
 
 @end
